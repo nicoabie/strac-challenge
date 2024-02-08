504c19bf6a4aa3447410f22fad3cc05d299112cc
Nico Gallinal <nicoabie@gmail.com>
2024-02-08 19:01:14 -0300

    remove global watchFileInterval

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { listFiles, getFile, getUsersWithFilePermission } = require('./drive')
 
 const PORT = 3000;
 const SEND_INTERVAL = 2000;
-let watchFileInterval;
 
 const express = require('express');
 const { renderFiles, renderWatchPage } = require('./render');
@@ -24,13 +23,12 @@ app.get('/download-file/:fileId', (req, res) => {
 
 app.get('/watch-file/:fileId', (req, res) => {
   const fileId = req.params.fileId;
-  clearInterval(watchFileInterval);
   res.writeHead(200, {
     'Cache-Control': 'no-cache',
     Connection: 'keep-alive',
     'Content-Type': 'text/event-stream',
   });
-  watchFileInterval = setInterval(() => {
+  const watchFileInterval = setInterval(() => {
     authorize().then((authClient) => getUsersWithFilePermission(authClient, fileId)).then((users) => {
       res.write(`data: ${JSON.stringify(users)}\n\n`);
     });
